98f8aeada66337156877e153c7516723c76c99a9
Cristina1580 <147286866+Cristina1580@users.noreply.github.com>
2026-02-09 17:39:34 -0300

    Refactor form submission and message handling
    Trimmed the 'nome' input value and improved message formatting for scheduling.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-document.getElementById('agendamentoForm').addEventListener('submit', function(e) {
+document.getElementById('agendamentoForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const nome = document.getElementById('nome').value;
+    const nome = document.getElementById('nome').value.trim();
     const servico = document.getElementById('servico').value;
     const periodo = document.getElementById('periodo').value;
-    const telefone = "5512987054594"; //
+    const telefone = "5512987054594";
 
     let mensagem;
 
     if (servico === "Saber o Valor da Consulta") {
         mensagem = `Olá Lótus Optometria! Me chamo ${nome} e gostaria de saber o valor da consulta.`;
     } else {
-        mensagem = `Olá Lótus Optometria! Gostaria de agendar:%0A%0A` +
-                   `*Paciente:* ${nome}%0A` +
-                   `*Serviço:* ${servico}%0A` +
-                   `*Período:* ${periodo}`;
+        mensagem =    
+`Olá Lótus Optometria! Gostaria de agendar:
+𝗣𝗮𝗰𝗶𝗲𝗻𝘁𝗲: ${nome}
+𝗦𝗲𝗿𝘃𝗶ç𝗼: ${servico}
+𝗣𝗲𝗿í𝗼𝗱𝗼: ${periodo}
+Qual horário disponível?`;
     }
 
-    window.open(`https://wa.me/${telefone}?text=${mensagem}`, '_blank');
-});
+        const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+
+        window.open(url, '_blank');
+    });
